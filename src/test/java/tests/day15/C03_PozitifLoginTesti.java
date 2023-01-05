4f4e156d379ae2e053da41c19f911c5a83fb3b87
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-01-05 21:15:08 +0100

    15.gun sonu

---
+++ src/test/java/tests/day15/C03_PozitifLoginTesti.java
@@ -0,0 +1,28 @@
+package tests.day15;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+
+public class C03_PozitifLoginTesti {
+    @Test
+    public void test01(){
+
+        // 1- https://www.qualitydemy.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        // 2- login linkine basin
+        QualitydemyPage qualitydemyPage=new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        // 3- Kullanici email'i olarak valid email girin
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("user_1106147@login.com");
+        // 4- Kullanici sifresi olarak valid sifre girin
+        qualitydemyPage.passwordKutusu.sendKeys("31488081");
+        // 5- Login butonuna basarak login olun
+        qualitydemyPage.loginButonu.click();
+        // 6- Basarili olarak giris yapilabildigini test edin
+        Assert.assertTrue(qualitydemyPage.basariliGirisCoursesLinki.isDisplayed());
+
+        Driver.closeDriver();
+    }
+}
---
+++ src/test/java/tests/day15/C04_NegativeLoginTesti.java
@@ -0,0 +1,55 @@
+package tests.day15;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+
+public class C04_NegativeLoginTesti {
+    // 3 test method'u olusturun
+    // 1.de yanlis email, dogru sifre
+    // 2.de dogru email yanlis sifre
+    // 3.de yanlis email, yanlis sifre ile giris yapmayi deneyin
+    // giris yapilamadigini test edin
+    QualitydemyPage qualitydemyPage= new QualitydemyPage();
+
+    @Test
+    public void yanlisEmailTesti(){
+
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("mehmet@abc.com");
+        qualitydemyPage.passwordKutusu.sendKeys("31488081");
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Driver.closeDriver();
+    }
+
+
+    @Test
+    public void yanlisPasswordTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("user_1106147@login.com");
+        qualitydemyPage.passwordKutusu.sendKeys("123456");
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Driver.closeDriver();
+
+    }
+
+
+    @Test
+    public void yanlisEmailYanlisPasswordTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("mehmet@abc.com");
+        qualitydemyPage.passwordKutusu.sendKeys("123456");
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Driver.closeDriver();
+    }
+}
