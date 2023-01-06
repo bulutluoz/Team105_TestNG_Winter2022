f75bc1972b8bf9f82d1fc87a9ea14ac51154e6f1
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-01-06 20:06:02 +0100

    16.gun sonu

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,8 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -12,9 +14,33 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
+        String browser= ConfigReader.getProperty("browser");
         if(driver==null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            switch (browser) {
+
+                case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+
+                case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+
+                case "safari" :
+                WebDriverManager.safaridriver().setup();
+                driver= new SafariDriver();
+                break;
+
+                default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+
+            }
+
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
